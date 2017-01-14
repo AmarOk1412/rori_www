34453775ac6b2c567fb8edef0ba239e03e26e1b7
AmarOk <blin.sebastien@enconn.fr>
2017-01-14 11:36:35 -0500

    fix path

--- add_module.php
+++ add_module.php
@@ -12,7 +12,7 @@ if (isset($_POST['type'])) {
   $module = json_decode($_POST['json_module']);
   array_push($modules, $module);
   echo json_encode($modules, JSON_PRETTY_PRINT);
-  $myfile = fopen($module_file, "w+") or die("Unable to open file (write)!");
+  $myfile = fopen($module_path, "w+") or die("Unable to open file (write)!");
   fwrite($myfile, str_replace("\\/", "/", json_encode($modules, JSON_PRETTY_PRINT)));
   fclose($myfile);
   header('Location: configure.php');
